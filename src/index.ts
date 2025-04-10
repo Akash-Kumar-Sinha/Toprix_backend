9d520273801db3ecab2a0d9f6bd9537e5c0a40da
Akash-Kumar-Sinha <akashkumarsinha304@gmail.com>
2025-04-10 21:02:52 +0530

    first commit

---
+++ src/index.ts
@@ -0,0 +1,37 @@
+import express, { Express } from "express";
+import dotenv from "dotenv";
+import session from "express-session";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+dotenv.config();
+const app: Express = express();
+const port = process.env.PORT || 3000;
+
+const SESSION_SECRET_KEY = process.env.SESSION_SECRET_KEY;
+
+if (!SESSION_SECRET_KEY) {
+  throw new Error("Missing SESSION_SECRET_KEY. Check the .env file.");
+}
+
+app.use(cors({ credentials: true, origin: "http://localhost:5173" }));
+
+app.use(
+  session({
+    secret: SESSION_SECRET_KEY,
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`);
+});
